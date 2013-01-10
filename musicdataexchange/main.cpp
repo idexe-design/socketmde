e7536c388aaf3c5c4c8e648d062659b5b7fa41c6
Kevin Dreßler <Kevin@MacBuch.local>
2013-01-10 13:28:53 +0100

    Initial Commit

---
+++ musicdataexchange/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  musicdataexchange
+//
+//  Created by Kevin Dreßler on 1/10/13.
+//  Copyright (c) 2013 Kevin Dreßler. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
